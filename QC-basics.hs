22aba25d54d7f689031a52205f631ea8403d3b19
Simon Buist <hashcollide@gmail.com>
2012-12-13 09:53:16 +0000

    Real World Haskell QC Tut

---
+++ QC-basics.hs
@@ -0,0 +1,39 @@
+{- 
+Real World Haskell 
+Chapter 11: Testing and quality assurance
+
+QuickCheck Basics
+http://book.realworldhaskell.org/read/testing-and-quality-assurance.html
+-}
+
+import Test.QuickCheck
+import Data.List
+
+qsort :: Ord a => [a] -> [a]
+qsort []     = []
+qsort (x:xs) = qsort lhs ++ [x] ++ qsort rhs
+    where lhs = filter  (< x) xs
+          rhs = filter (>= x) xs
+          
+prop_idempotent :: [Integer] -> Bool
+prop_idempotent xs = qsort (qsort xs) == qsort xs
+
+prop_minimum xs = not (null xs) ==> 
+                  head (qsort xs) == minimum xs
+                  
+prop_ordered xs = ordered (qsort xs)
+    where ordered []       = True
+          ordered [x]      = True
+          ordered (x:y:xs) = x <= y && ordered (y:xs)
+         
+prop_permutation xs = permutation xs (qsort xs)
+    where permutation xs ys = null (xs \\ ys) && null (ys \\ xs)
+          
+prop_maximum xs = not (null xs) ==>
+                  last (qsort xs) == maximum xs
+                  
+prop_append xs ys = not (null xs) ==>
+                    not (null ys) ==>
+                    head (qsort (xs ++ ys)) == min (minimum xs) (minimum ys)                    
+                    
+prop_sort_model xs = sort xs == qsort xs
