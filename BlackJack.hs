31538ddd12828fb37d6bac0e2d9bce3f17d6a44f
= <=>
2012-12-15 19:51:47 +0000

    Tidy up

--- BlackJack.hs
+++ BlackJack.hs
@@ -83,54 +83,63 @@ prop_size_onTopOf :: Hand -> Hand -> Bool
 prop_size_onTopOf h1 h2 = 
   size h1 + size h2 == size (h1 <+ h2)
 
+-- a full deck of 52 cards
 fullDeck :: Hand
 fullDeck = fullHand Hearts <+ (fullHand Spades <+ (fullHand Diamonds <+ fullHand Clubs))
 
+-- given a suit, return a full hand for that suit
 fullHand :: Suit -> Hand
 fullHand s = fullNums s <+ (Add (Card Jack s)
                             (Add (Card King s)
                              (Add (Card Queen s)
                               (Add (Card Ace s) Empty))))
 
+-- given a suit, return a hand of all the numeric cards for that suit
 fullNums :: Suit -> Hand
 fullNums s = toHand $ map (\x -> (Card (Numeric x) s)) [2..10]
 
+-- draw a card into the players hand from the deck, and return the new values of (hand, deck)
 draw :: Hand -> Hand -> (Hand, Hand)
 draw Empty _   = error "draw: The deck is empty"
 draw (Add c h1) h2 = (h1, (Add c h2))
 
+-- play a bank move according to the rules of Black Jack
 playBank :: Hand -> Hand
 playBank deck = playBank' deck Empty
 
+-- a helper function for playBank which keeps on drawing from the deck
+-- until the bank's hand value is > 16
 playBank' :: Hand -> Hand -> Hand
 playBank' deck bank = if value bank' <= 16 
                       then playBank' deck' bank'
 			else bank
                         where (deck', bank') = draw deck bank
 
--- removes the nth card from the hand and returns the card and the remaining hand
+-- given a hand and a number n, remove the nth card from the hand 
+-- resulting in a tuple (card, hand_without_card)
 removeCard :: Hand -> Integer -> (Card, Hand)
 removeCard Empty 0 = error "removeCard: card not in hand"
 removeCard hand  n = removeCard' Empty hand n
 
 
---             Before  Current n
+-- a helper function for removeCard which builds the remaining_hand into before
 removeCard' :: Hand -> Hand -> Integer -> (Card, Hand)
 removeCard' before (Add c h) 0 = (c, (before    <+ h))
 removeCard' before (Add c Empty) n =  (c, before)
 removeCard' before (Add c h) n = removeCard' 
                                  ((Add c Empty) <+ before) h (n-1)
                                  
+-- given a PRNG and a hand, shuffle the hand
 shuffle :: StdGen -> Hand -> Hand
 shuffle g Empty = Empty
 shuffle g h     = shuffle' g Empty h
                   
+-- a helper function for shuffle which stores the shuffled result so far
 shuffle':: StdGen -> Hand -> Hand -> Hand
 shuffle' g result Empty = result
 shuffle' g result curr = shuffle' g' ((Add c Empty) <+ result) h 
   where (n,g') = randomR(0, (size curr)) g
         (c,h) = removeCard curr n
-
 
 prop_shuffle_sameCards :: StdGen -> Card -> Hand -> Bool
 prop_shuffle_sameCards g c h =
@@ -140,6 +149,7 @@ prop_size_shuffle :: StdGen -> Hand -> Bool
 prop_size_shuffle g h =
   size h == size (shuffle g h)
 
+-- given a card, is the card in the hand?
 belongsTo :: Card -> Hand -> Bool
 c `belongsTo` Empty = False
 c `belongsTo` (Add c' h) = c == c' || c `belongsTo` h
