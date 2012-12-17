ac69471cf86f8561fb1bdc6d60c51d867633ccdc
= <=>
2012-12-17 00:20:30 +0000

    Add lab4

---
+++ lab4.hs
@@ -0,0 +1,45 @@
+import Test.QuickCheck
+import Control.Monad
+
+look :: Eq a => a -> [(a,b)] -> Maybe b
+look x []           = Nothing
+look x ((x',y):xys)
+  | x == x'         = Just y
+  | otherwise       = look x xys
+                      
+                      
+prop_LookNothing x t = 
+  if look x t == Nothing
+     then not (x `elem` (map fst t))
+  else True
+       
+prop_LookJust x y t =
+  if look x t == Just y
+     then (x,y) `elem` t
+  else True
+       
+prop_Look x y t = prop_LookNothing x t || prop_LookJust x y t
+
+sequence' :: Monad m => [m a] -> m [a]
+sequence' []     = return []
+sequence' (m:ms) = do val <- m
+                      vals <- sequence' ms
+                      return (val:vals)
+                      
+-- can also be written
+-- sequence' = foldr (liftM2 (:)) (return [])   
+
+files = ["hello.txt","world.txt"]
+
+onlyIf :: Monad m => Bool -> m () -> m ()
+onlyIf True  m = do m 
+onlyIf False m = return ()
+
+game :: IO ()
+-- use the binary chop algorithm
+-- start off by picking half of possible range
+-- e.g. if possible range is [1..100], pick 50
+-- if higher, go for half of [51..100]
+-- if lower, go for [1..49]
+-- etc
+game = undefined 
