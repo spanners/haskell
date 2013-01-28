739c92139ee501fedb43b9ffecd380199864ba35
Simon Buist <hashcollide@gmail.com>
2013-01-28 23:16:31 +0000

    Mostly formatting. Simplified win condition

--- Hangman.hs
+++ Hangman.hs
@@ -16,22 +16,22 @@ randomWord :: IO String
 randomWord = do 
   wlist <- readFile wordFile
   let ws = words wlist
-  n <- randomRIO (0,length ws - 1)
+  n <- randomRIO (0, length ws - 1)
   return (ws !! n)
 
 gameLoop :: String -> String -> IO()
-gameLoop w g | win  = showWin
-             | lose = showLose
+gameLoop w g | win       = showWin
+             | lose      = showLose
              | otherwise = 
    do displayStatus
       guesses <- getLine
       gameLoop w (g `union` take lives guesses)  
   where
-    win   = and [c `elem` g | c <- w] -- all (`elem` g) w
-    lose  = lives <= 0
-    lives = guessLimit - length (g \\ w)
-    showLose = putStrLn $ "You die! The word was " ++ w
-    showWin  = putStrLn $ "Win! You guessed " ++ w 
+    win           = all (`elem` g) w
+    lose          = lives <= 0
+    lives         = guessLimit - length (g \\ w)
+    showLose      = putStrLn $ "You die! The word was " ++ w
+    showWin       = putStrLn $ "Win! You guessed " ++ w 
                              ++ " with " ++ show lives 
                              ++ " lives remaining"
     displayStatus = do putStrLn [if c `elem` g then c else '_' | c <- w] 
