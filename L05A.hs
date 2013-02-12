efce02daf3b6f5c635c905cc0e0a843aa92993e2
Simon Buist <hashcollide@gmail.com>
2013-02-12 18:38:16 +0000

    Add L05 files

---
+++ L05A.hs
@@ -0,0 +1,110 @@
+-- L05A Monads 
+import Test.QuickCheck
+import Parsing
+import Data.Char
+
+blist = do 
+  char '{'
+  ds <- chain digit (char ',')
+  char '}'
+  return $ map digitToInt ds
+
+blist' = char '{' >> do 
+  ds <- chain digit (char ',')
+  char '}'
+  return $ map digitToInt ds
+
+blist'' = char '{' >> chain digit (char ',') >>= \ds -> 
+            char '}' >>
+              return (map digitToInt ds)
+
+
+
+prop_blist = 
+ parse blist "{1,2,3,4}xxx" == Just ([1,2,3,4], "xxx")
+
+doTwice i = do 
+  a <- i
+  b <- i
+  return (a,b)
+
+doTwice' i = i >>= \a -> 
+            i >>= \b -> 
+            return (a,b)
+
+-----------------------------------------
+{--- Example of another Monad: Maybe
+-- from the Prelude:
+ instance Monad Maybe where
+    return         = Just
+
+    Nothing  >>= f = Nothing
+    (Just x) >>= f = f x
+
+-----------------------------------------}
+
+type CarReg = String ; type PNr = String  
+type Name = String   ; type Address = String
+
+carRegister :: [(CarReg,PNr)]
+carRegister
+ = [("FYN 433","850219-1234"),
+    ("GYN 434","850219-1234"),
+    ("JBD 007","750408-0909")]
+
+nameRegister :: [(PNr,Name)]
+nameRegister 
+ = [("750408-0909","Dave")
+   ,("850219-1234","Bob") 
+   ,("890929-C234","Pierre")]             
+
+addressRegister :: [((Name,PNr),Address)]
+addressRegister = 
+  [(("Dave","750408-0909"),"42 Streetgatan\n Askim")
+  ,(("Bob","850219-1234") ,"1 Chalmers Av\n Gothenburg") ]
+
+
+billingAddress :: CarReg -> Maybe (Name, Address)
+-- given a registration number, 
+-- returns the name and address of owner, if defined. 
+
+billingAddress car = 
+ case lookup car carRegister of
+   Nothing -> Nothing
+   Just pnr -> case lookup pnr nameRegister of
+        Nothing -> Nothing
+        Just name -> 
+           case lookup (name,pnr) addressRegister of
+                Nothing -> Nothing
+                Just addr -> Just (name,addr)
+
+-- Monadic style:
+billingAddress' car = do 
+  pnr  <- lookup car carRegister
+  name <- lookup pnr nameRegister
+  addr <- lookup (name,pnr) addressRegister
+  return (name,addr)
+
+-- one step of de-sugaring:
+billingAddress'' car = 
+  lookup car carRegister >>= \pnr -> 
+ do 
+  name <- lookup pnr nameRegister
+  addr <- lookup (name,pnr) addressRegister
+  return (name,addr)
+
+test' = do 
+     return 32
+     x <- Nothing
+     return 42
+
+test'' =  
+     return 32 >>= \_ -> 
+     Nothing   >>= \x -> return 42
+
+ 
+
+-- The fail function gives an error by default
+test = do putStrLn "hello"
+          "42" <- getLine
+          putStrLn "World"
--- Parsing.hs
+++ Parsing.hs
@@ -1,47 +1,38 @@
-module RefactoredParser
+module Parsing
  ( Parser -- exports the type name but not the constructors
   ,parse,
   success,failure,sat,pmap,item,char,digit,
   (+++),(<:>),(>*>),(>->),(<-<),
-  oneOrMore,zeroOrMore,chain
- ) 
+  oneOrMore,zeroOrMore,chain ) 
 {----------------------
-Week 4B part II
+Week 5A part I
 
-Aim: reusable Parser combinators including 
- a new type for the Parser, 
- but no export of the constructor
+Same as RefactoredParser.hs but now
+Parser added to typeclass Monad
 ----------------------}
 where
 
 import Data.Char
 ------------------
 
--- take the old type (in ReadExpr) which was a type synonym which is just the same as the old type except now it has a constructor on the front
--- so it's the same but is hidden inside a constructor
 newtype Parser a = P (String -> Maybe (a,String))
 
--- Our library is exporting the type Parser, but not the function P
--- the only thing we'll be able to use to parse, is this parse function
 parse :: Parser a -> String -> Maybe(a,String)
 -- run a parser on a given string
--- pulls out the function from inside
 parse (P f) = f
 
 -------------------
 -- Basic Parsers, dependent on internal structure -- 
 -- success and fail
-failure :: Parser a 
--- always fails
-failure    = P $ \_ -> 
+failure :: Parser a -- always fails
+failure    = P $ \s -> 
              Nothing
-
+ 
 success :: a -> Parser a 
 -- succeeds without looking at the string
 success a  = P $ \s -> 
              Just (a,s)
 
-item :: Parser Char
 -- Parse any single character
 item  = P $ \s ->
         case s of 
@@ -82,24 +73,23 @@ sat p  = item >*> \c -> if p c then success c else failure
 char c = sat (==c)
 digit  = sat isDigit
 
--- pmap modifies the result of a parser with function f
--- e.g. parse (digitToInt `pmap` digit) "123"
+-- pmap modifies the result of a parser
+
 pmap :: (a -> b) -> Parser a -> Parser b
 pmap f p = p >*> \a -> success (f a)
 
--- throw away what p gave, and parse with q
--- e.g.  parse (char '{' >-> digit) "{123}"
 (>->) :: Parser a -> Parser b -> Parser b
 p >-> q = p >*> \_ -> q
 
--- parses with p, and then just parses with q to make sure it's there and throws it away
--- e.g.  parse (char '{' >-> digit <-< char '}') "{1}xxx"
 (<-<) :: Parser b -> Parser a -> Parser b
 p <-< q = p >*> \a -> q >-> success a
 
 (<:>):: Parser a -> Parser [a] -> Parser [a]
-p <:> q = p >*> \a -> pmap (a:) q
-
+p <:> q = -- p >*> \a -> pmap (a:) q
+  do a <- p
+     as <- q
+     return $ a:as
+  
 oneOrMore, zeroOrMore :: Parser a -> Parser [a]
 zeroOrMore p = oneOrMore p +++ success []
 oneOrMore p  = p <:> zeroOrMore p
@@ -110,3 +100,11 @@ chain p q = p <:> zeroOrMore (q >-> p)
 
 -- example: comma separated digits "1,2,3"
 diglist = chain digit (char ',') 
+
+
+
+
+
+instance Monad Parser where
+  (>>=)  = (>*>)
+  return = success
